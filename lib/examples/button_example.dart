d7999623cb69af979d7d53447648f81ea6519a3c
aidenZou <aidenZou@gmail.com>
2022-12-28 23:24:17 +0800

    feat: ButtonExample文档更新

--- lib/examples/button_example.dart
+++ lib/examples/button_example.dart
@@ -13,7 +13,7 @@ class ButtonExample extends StatelessWidget {
       children: <Widget>[
         CodeBox(
           title: '按钮类型',
-          description: '按钮有五种类型：主按钮、次按钮、(虚线按钮)、文本按钮和链接按钮。主按钮在同一个操作区域最多出现一次。',
+          description: '按钮有五种类型：主按钮、次按钮、虚线按钮、文本按钮和链接按钮。主按钮在同一个操作区域最多出现一次。',
           child: Space(
             children: [
               Button(
